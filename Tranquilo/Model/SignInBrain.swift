92c37c7d733bdc6d5eedde0c97db922d04c98475
Ashley Acevedo <40500769+ashleyrennee@users.noreply.github.com>
2022-11-19 15:28:01 -0700

    Initial Commit

---
+++ Tranquilo/Model/SignInBrain.swift
@@ -0,0 +1,14 @@
+//
+//  SignInUpBrain.swift
+//  Tranquilo
+//
+//  Created by Ashley Acevedo on 10/28/22.
+//
+
+import Foundation
+struct SignInBrain{
+    
+    
+    
+    
+}
---
+++ WeatherManager.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by Ashley Acevedo on 11/15/22.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import Foundation
